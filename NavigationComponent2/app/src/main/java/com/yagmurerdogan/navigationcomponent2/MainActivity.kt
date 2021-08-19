5946855aa1c27a613f2868d08a61e71586c394e2
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-19 15:09:33 +0300

    Set up appbar configurations to main activity

--- NavigationComponent2/app/src/main/java/com/yagmurerdogan/navigationcomponent2/MainActivity.kt
+++ NavigationComponent2/app/src/main/java/com/yagmurerdogan/navigationcomponent2/MainActivity.kt
@@ -7,13 +7,16 @@ import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +26,14 @@ class MainActivity : AppCompatActivity() {
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.findNavController()
 
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.homeFragment, R.id.searchFragment)
+        )
+
         setSupportActionBar(toolbar)
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottom_nav.setupWithNavController(navController)
 
     }
 
