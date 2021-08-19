93645252c96943d730d2342f67916b0fe6c9cae7
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-19 11:08:39 +0300

    Get args and set them to text views

--- NavigationComponent2/app/src/main/java/com/yagmurerdogan/navigationcomponent2/WelcomeFragment.kt
+++ NavigationComponent2/app/src/main/java/com/yagmurerdogan/navigationcomponent2/WelcomeFragment.kt
@@ -1,6 +1,19 @@
 package com.yagmurerdogan.navigationcomponent2
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import kotlinx.android.synthetic.main.fragment_welcome.*
 
-class WelcomeFragment: Fragment(R.layout.fragment_welcome) {
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+
+    private val args: WelcomeFragmentArgs by navArgs()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        text_view_user_name.text = args.username
+        text_view_password.text = args.password
+    }
 }
