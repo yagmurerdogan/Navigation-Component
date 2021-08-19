41cfbb42dff96446dc5c2429a89d1ee2d07d31a7
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-19 19:26:05 +0300

    Set up nav view to MainActivity.kt

--- NavigationComponent2/app/src/main/java/com/yagmurerdogan/navigationcomponent2/MainActivity.kt
+++ NavigationComponent2/app/src/main/java/com/yagmurerdogan/navigationcomponent2/MainActivity.kt
@@ -7,10 +7,7 @@ import android.view.MenuItem
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.onNavDestinationSelected
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
+import androidx.navigation.ui.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -27,13 +24,15 @@ class MainActivity : AppCompatActivity() {
         navController = navHostFragment.findNavController()
 
         appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.homeFragment, R.id.searchFragment)
+            setOf(R.id.homeFragment, R.id.searchFragment),
+            drawer_layout
         )
 
         setSupportActionBar(toolbar)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         bottom_nav.setupWithNavController(navController)
+        nav_view.setupWithNavController(navController)
 
     }
 
@@ -53,6 +52,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return navController.navigateUp() || super.onSupportNavigateUp()
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
