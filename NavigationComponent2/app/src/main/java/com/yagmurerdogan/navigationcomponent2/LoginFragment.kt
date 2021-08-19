264400e82384e8b741984425831aef3ef6150836
Yağmur Erdoğan <yagmur.erdogan@hepsiburada.com>
2021-08-19 11:08:14 +0300

    Pass username and password arguments to welcome fragment

--- NavigationComponent2/app/src/main/java/com/yagmurerdogan/navigationcomponent2/LoginFragment.kt
+++ NavigationComponent2/app/src/main/java/com/yagmurerdogan/navigationcomponent2/LoginFragment.kt
@@ -6,13 +6,18 @@ import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_login.*
 
-class LoginFragment: Fragment(R.layout.fragment_login) {
+class LoginFragment : Fragment(R.layout.fragment_login) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         button_confirm.setOnClickListener {
-            val action = LoginFragmentDirections.actionLoginFragmentToWelcomeFragment()
+
+            val username = edit_text_username.text.toString()
+            val password = edit_text_password.text.toString()
+
+            val action =
+                LoginFragmentDirections.actionLoginFragmentToWelcomeFragment(username, password)
             findNavController().navigate(action)
         }
     }
